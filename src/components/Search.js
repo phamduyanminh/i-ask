a37d6b70a44c6f03d6d197eec02757b11f128d5c
Minh Pham <phamduya@sheridancollege.ca>
2022-09-21 17:45:14 -0400

    Init the project and created a search bar

---
+++ src/components/Search.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Search = () => {
+    return(
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <input placeholder="Enter search term" className="input"/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Search;
